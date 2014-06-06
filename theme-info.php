40ea7b48c28e1b10039ff6a855c4f79d83d36bd4
jon4god <jon4god@me.com>
2014-06-06 11:25:41 +0400

    Моя тема для блога Эгеи.

---
+++ theme-info.php
@@ -0,0 +1,12 @@
+<?php return array (
+
+  'display_name' => array (
+    'en' => 'Starcoms',
+    'ru' => 'Starcoms',
+  ),
+
+  'max_image_width' => '768',
+  
+  //'based_on' => 'classical',
+
+); ?>
